e7286e78c4b3de8b3ed1e84f2b4e19a6328e5a6e
Chocolateandmilkwin <nicknielsen1995@hotmail.com>
2023-01-23 17:36:51 +0300

    Fixed wrong output type from svg generators

--- src/shared.ts
+++ src/shared.ts
@@ -2,11 +2,11 @@ export let generateFunction = (name: string, icon: string) => {
     let svg: SVGSVGElement;
     return function (this: any) {
         if (svg) {
-            return svg.cloneNode(true);
+            return <SVGSVGElement>svg.cloneNode(true);
         } else {
             svg = <SVGSVGElement>(new DOMParser().parseFromString(icon, 'image/svg+xml')).firstChild;
             svg.setAttribute('icon', name);
-            return svg.cloneNode(true);
+            return <SVGSVGElement>svg.cloneNode(true);
         }
     }
 }
